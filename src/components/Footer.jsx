59c8b1aa0593e4592bb24ed95904ad28eb0bfa43
zulfa Ramadhan <zulfar436@gmail.com>
2024-04-22 10:07:12 +0300

    made changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer() {
+    return (
+      <>
+        <footer className="bg-[#000] flex flex-col sm:flex-row">
+          <div className="container mx-auto">
+            <div className="flex list-none gap-9 leading-[3em]">
+              <div>
+                <h2 className="head font-semibold text-[1.5em]">Contact us</h2>
+                <div className="flex text-white gap-5">
+                  <li>contact@culture-clan.com</li>
+                </div>
+                <div className="flex text-white gap-6">
+                  <li>254-700-333-333</li>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </>
+    );
+  }
+  export default Footer;
