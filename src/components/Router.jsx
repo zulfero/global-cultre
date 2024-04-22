fc1f93c66f56156fd4aa9097d2a6bbb0219cf1a0
zulfa Ramadhan <zulfar436@gmail.com>
2024-04-22 09:56:02 +0300

    made changes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,1 +1,42 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Layout from "../pages/Layout";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <Login />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <NotFound />
+            </Layout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default Router;
